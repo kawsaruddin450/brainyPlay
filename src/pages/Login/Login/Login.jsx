5a1ec843a986249a3d072e949ba90032019cd8f9
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-26 23:37:20 +0600

    add login and register form

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -1,9 +1,30 @@
+import { Link } from "react-router-dom";
 
 
 const Login = () => {
     return (
-        <div>
-            <h1>Login page coming soon...</h1>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Login now!</h1>
+                </div>
+                <div className=" bg-base-100 w-full shadow-2xl">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <input type="password" placeholder="password" name="password" className="input input-bordered" required />
+                            <label className="label">
+                                Don't have an account? <Link to='/register' className="label-text-alt link link-hover text-orange-500 hover:text-orange-500">Register Now</Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn bg-orange-500 hover:bg-orange-600 text-white">Login</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,9 +1,33 @@
+import { Link } from "react-router-dom";
 
 
 const Register = () => {
     return (
-        <div>
-            <h1>Register page coming soon...</h1>
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl font-bold">Register now!</h1>
+                </div>
+                <div className=" bg-base-100 w-full shadow-2xl">
+                    <form className="card-body">
+                        <div className="form-control">
+                            <input type="text" placeholder="name" name="name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <input type="email" placeholder="email" name="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <input type="password" placeholder="password" name="password" className="input input-bordered" required />
+                            <label className="label">
+                                Already have an account? <Link to='/login' className="label-text-alt link link-hover text-orange-500 hover:text-orange-500">Login Now</Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn bg-orange-500 hover:bg-orange-600 text-white">Register</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     );
 };
