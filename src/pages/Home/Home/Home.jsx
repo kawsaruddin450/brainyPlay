88dcede2f9319283e769106ae8d8c82ed9c21302
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-26 13:08:56 +0600

    add homepage baner section

--- src/pages/Home/Home/Home.jsx
+++ src/pages/Home/Home/Home.jsx
@@ -1,9 +1,26 @@
-
+import background from '../../../assets/homebanner.jpg'
 
 const Home = () => {
     return (
         <div>
-            <h1 className='text-5xl'>Home Page Coming Soon...</h1>
+            <div className="">
+                <div
+                    className="hero min-h-screen"
+                    style={{
+                        backgroundImage: `url(${background})`,
+                    }}>
+                    <div className="hero-overlay bg-opacity-60"></div>
+                    <div className="hero-content text-white text-center">
+                        <div className="max-w-md">
+                            <h1 className="mb-5 text-7xl font-bold font-nerko">Let's Explore Together!</h1>
+                            <p className="mb-5">
+                                Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                            </p>
+                            <button className="btn bg-orange-500 hover:bg-orange-600 text-white border-0">Get Started</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ export default {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'nerko': ["Nerko One", 'cursive']
+    }
   },
   plugins: [
     // eslint-disable-next-line
