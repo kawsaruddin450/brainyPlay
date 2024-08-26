5ec01765354b3065bcf9671036e27f24934a739f
kawsar-uddin <kawsaruddin450@gmail.com>
2024-08-26 16:22:23 +0600

    dynamic toy card customization

--- src/pages/Home/Shop/Shop.jsx
+++ src/pages/Home/Shop/Shop.jsx
@@ -5,18 +5,68 @@ import ToyCard from '../ToyCard/ToyCard';
 
 
 const Shop = () => {
-    const [toys, setToys] = useState([]);
+    const [AllToys, setAllToys] = useState([]);
+    const [toys, setToys] = useState(AllToys);
+
+    const filterCategory = (index) => {
+        if (index === 0) {
+            setToys(AllToys);
+        }
+        else if (index === 1) {
+            const newToys = toys.filter(toy => toy.category === "Science")
+            setToys(newToys);
+        }
+        else if (index === 2) {
+            const newToys = toys.filter(toy => toy.category === "Language")
+            setToys(newToys);
+        }
+        else if (index === 3) {
+            const newToys = toys.filter(toy => toy.category === "Engineering")
+            setToys(newToys);
+        }
+        else if (index === 4) {
+            const newToys = toys.filter(toy => toy.category === "STEM")
+            setToys(newToys);
+        }
+        else if (index === 5) {
+            const newToys = toys.filter(toy => toy.category === "Mathematics")
+            setToys(newToys);
+        }
+        else if (index === 6) {
+            const newToys = toys.filter(toy => toy.category === "Interactive Learning")
+            setToys(newToys);
+        }
+        else if (index === 7) {
+            const newToys = toys.filter(toy => toy.category === "Logic")
+            setToys(newToys);
+        }
+        else if (index === 8) {
+            const newToys = toys.filter(toy => toy.category === "Cognitive Development")
+            setToys(newToys);
+        }
+        else if (index === 9) {
+            const newToys = toys.filter(toy => toy.category === "Building")
+            setToys(newToys);
+        }
+    }
 
     useEffect(() => {
         fetch('/toys.json')
             .then(res => res.json())
-            .then(data => setToys(data))
+            .then(data => {
+                setAllToys(data);
+                setToys(data);
+            })
     }, [])
+
+    const changeIndex = (index) => {
+        filterCategory(index);
+    }
 
     return (
         <div className='my-28'>
             <h1 className='text-7xl font-bold text-center mb-12'>Shop Now</h1>
-            <Tabs>
+            <Tabs defaultIndex={0} onSelect={(index) => changeIndex(index)}>
                 <TabList>
                     <Tab>All</Tab>
                     <Tab>Science</Tab>
@@ -32,7 +82,7 @@ const Shop = () => {
 
                 {/* All */}
                 <TabPanel>
-                    <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
+                    <div className='mt-8 lg:container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-5'>
                         {
                             toys.map(toy => <ToyCard
                                 key={toy.id}
@@ -43,39 +93,102 @@ const Shop = () => {
                 </TabPanel>
                 {/* Science */}
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className='mt-8 lg:container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
+                        {
+                            toys.map(toy => <ToyCard
+                                key={toy.id}
+                                toy={toy}
+                            ></ToyCard>)
+                        }
+                    </div>
                 </TabPanel>
                 {/* Language */}
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className='mt-8 lg:container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
+                        {
+                            toys.map(toy => <ToyCard
+                                key={toy.id}
+                                toy={toy}
+                            ></ToyCard>)
+                        }
+                    </div>
                 </TabPanel>
                 {/* Engineering */}
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className='mt-8 lg:container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
+                        {
+                            toys.map(toy => <ToyCard
+                                key={toy.id}
+                                toy={toy}
+                            ></ToyCard>)
+                        }
+                    </div>
                 </TabPanel>
                 {/* Stem */}
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className='mt-8 lg:container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
+                        {
+                            toys.map(toy => <ToyCard
+                                key={toy.id}
+                                toy={toy}
+                            ></ToyCard>)
+                        }
+                    </div>
                 </TabPanel>
                 {/* Mathematics */}
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className='mt-8 lg:container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
+                        {
+                            toys.map(toy => <ToyCard
+                                key={toy.id}
+                                toy={toy}
+                            ></ToyCard>)
+                        }
+                    </div>
                 </TabPanel>
                 {/* Interactive Learning */}
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className='mt-8 lg:container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
+                        {
+                            toys.map(toy => <ToyCard
+                                key={toy.id}
+                                toy={toy}
+                            ></ToyCard>)
+                        }
+                    </div>
                 </TabPanel>
                 {/* Logic */}
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className='mt-8 lg:container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
+                        {
+                            toys.map(toy => <ToyCard
+                                key={toy.id}
+                                toy={toy}
+                            ></ToyCard>)
+                        }
+                    </div>
                 </TabPanel>
                 {/* Cognitive Development */}
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className='mt-8 lg:container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
+                        {
+                            toys.map(toy => <ToyCard
+                                key={toy.id}
+                                toy={toy}
+                            ></ToyCard>)
+                        }
+                    </div>
                 </TabPanel>
                 {/* Building */}
                 <TabPanel>
-                    <h2>Any content 2</h2>
+                    <div className='mt-8 lg:container mx-auto grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-5'>
+                        {
+                            toys.map(toy => <ToyCard
+                                key={toy.id}
+                                toy={toy}
+                            ></ToyCard>)
+                        }
+                    </div>
                 </TabPanel>
             </Tabs>
         </div>
--- src/pages/Home/ToyCard/ToyCard.jsx
+++ src/pages/Home/ToyCard/ToyCard.jsx
@@ -1,3 +1,5 @@
+import { FaRegStar, FaStar } from "react-icons/fa";
+import Rating from "react-rating";
 
 
 const ToyCard = ({ toy }) => {
@@ -9,9 +11,26 @@ const ToyCard = ({ toy }) => {
                     src={image}
                     alt="Toy" />
             </figure>
-            <div className="card-body">
-                <h2 className="card-title">{name}</h2>
-                <p>{description}</p>
+            <div className="p-5">
+                <h2 className="card-title mt-3">{name}</h2>
+                <p className="my-3">{description}</p>
+                <p className="text-xl">Category: {category}</p>
+                <div className="flex justify-between text-xl my-3">
+                    <p className="font-bold">Price: <span>${price}</span></p>
+                    <p>
+                        <span className=" mr-4">{rating}</span>
+                        <span>
+                            <Rating
+                                className="text-warning"
+                                readonly
+                                placeholderRating={rating}
+                                emptySymbol={<FaRegStar></FaRegStar>}
+                                placeholderSymbol={<FaStar></FaStar>}
+                                fullSymbol={<FaStar></FaStar>}
+                            />
+                        </span>
+                    </p>
+                </div>
                 <div className="card-actions justify-end">
                     <button className="btn bg-orange-500 hover:bg-orange-600 text-white border-0">View Details</button>
                 </div>
